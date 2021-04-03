a9879b77124687f19d2c8f5e81520ef85314cee6
Yamid Huertas <yamid.huertas@MacBook-Air-de-Yamid.local>
2021-04-03 10:29:16 -0500

    Cors

--- server/src/app.js
+++ server/src/app.js
@@ -3,10 +3,11 @@
 const morgan = require("morgan"); //Permite ver la interacción entre el servidor y los clientes (el navegador). (HTTP request logger middleware for node.js)
 const express = require("express");
 const app = express(); //app contien toda la funcionalidad del server
-
+const cors = require('cors')
 app.set('port', process.env.PORT || 3000); //definciòn del puerto por el que escucharà el servidor.
 
 //middleware
+app.use(cors)
 app.use(morgan('dev')); //para indicar como queremos ver los mensajes en consola,hay varias: skip , combined, stream, common, short, tiny, etc
 app.use(express.json()); //Perite interpretar los json que se envían desde el navegador al servidor.
 app.use(express.urlencoded({ //esta junto con .json se usan para POST and PUT para indicarle al servidor que puede guardar la información enviada en el req.body. Object as strings or arrays
